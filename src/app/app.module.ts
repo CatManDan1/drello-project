8efb9ca5335dc6067a3aa992d9b9d2bc150f53e1
CatManDan <daniel.nussey1@gmail.com>
2022-09-15 09:42:55 +0200

    more things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskListComponent,
+    DashboardComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
